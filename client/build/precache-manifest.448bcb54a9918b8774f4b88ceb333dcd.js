84b5f76416cf6d1fd39e44d5bcc642a6c884dbe8
BluCreativeSolar <Convict_Player@hotmail>
2020-06-11 11:24:10 -0400

    ok

---
+++ client/build/precache-manifest.448bcb54a9918b8774f4b88ceb333dcd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e232a7820cee976948bee619dc0cece",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5df7f68fe68f60672190",
+    "url": "./static/css/main.0209873b.chunk.css"
+  },
+  {
+    "revision": "128823b69cee419e5fc1",
+    "url": "./static/js/2.e115977c.chunk.js"
+  },
+  {
+    "revision": "603816eec26c1aff4a38bef8d08d40a6",
+    "url": "./static/js/2.e115977c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5df7f68fe68f60672190",
+    "url": "./static/js/main.5c3e3d6e.chunk.js"
+  },
+  {
+    "revision": "f35dbb30bb93cebd5c82",
+    "url": "./static/js/runtime-main.f63e6028.js"
+  },
+  {
+    "revision": "4b31330b20e3cff3e49d540e4e7175e2",
+    "url": "./static/media/showcase.4b31330b.jpg"
+  }
+]);
